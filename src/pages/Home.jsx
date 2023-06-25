7db2063f060377649eaf319fbb3bea9740ac1bb8
HhDreamer <hh.dreamer@gmail.com>
2023-06-25 09:36:29 +0200

    commit add react helmet.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Helmet } from "react-helmet";
 import Leaf from "../components/Leaf";
 import Modal from "../components/Modal";
 
@@ -7,6 +8,22 @@ const Home = () => {
 
   return (
     <>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>Les Amis de Tobie</title>
+        <meta
+          name='description'
+          content='Bienvenue sur le site officiel de « Les Amis de Tobie ». Embarquez avec nous dans cette aventure unique et passionnante! Restez informé(e) des nouvelles de notre projet.'
+        />
+        <meta property='og:title' content='Les Amis de Tobie' />
+        <meta
+          property='og:description'
+          content='Bienvenue sur le site officiel de « Les Amis de Tobie ». Embarquez avec nous dans cette aventure unique et passionnante! Restez informé(e) des nouvelles de notre projet.'
+        />
+        <meta property='og:image' content='/tobie-icon.webp' />
+        <meta property='og:url' content='https://www.lesamisdetobie.fr' />
+        <link rel='canonical' href='https://www.lesamisdetobie.fr' />
+      </Helmet>
       <div className=''>
         <Modal showModal={showModal} setShowModal={setShowModal} />
         <Leaf showModal={showModal} setShowModal={setShowModal} />
